7d86a2c3b06d8103857df25ed70445077095980e
Moise Tshiibwabwa <moisetshi@hotmail.com>
2022-03-15 22:11:37 +0100

    Almost

--- JS/script.js
+++ JS/script.js
@@ -62,3 +62,31 @@ function closeElectionResults() {
     document.getElementById("electionResults").style.display = "none";
     document.getElementById("election").style.display = "flex";
 }
+
+/*--------------------------- Countdown ----------------------------*/
+
+var countDate =  new Date('Mars 18, 2022 00:00:00').getTime();
+
+function newYear() {
+    var now = new Date().getTime();
+    gap = countDate - now;
+
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    newYear();
+},1000);
